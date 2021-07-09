8f54ba2016a6a6857735cd5d03556a4f0e7c3f4b
Ludin Nento <ludyyn@gmail.com>
2021-07-09 22:21:08 +0800

    new branch

--- private/main.go
+++ private/main.go
@@ -24,7 +24,14 @@ func main() {
 	}
 	block, _ := pem.Decode(buf)
 	key, _ := x509.ParsePKCS1PrivateKey(block.Bytes)
-	fmt.Println(key.D.Bytes())
 
+	priv, err1 := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err1 != nil {
+		log.Println(err1)
+		os.Exit(1)
+	}
+
+	fmt.Println(key.D.Bytes(), key.Size())
+	fmt.Println(priv)
 
 }
